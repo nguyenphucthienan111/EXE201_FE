f640c575be30848ff3f9645bb56f68bbdf421f51
nguyenphucthienan111 <155414257+nguyenphucthienan111@users.noreply.github.com>
2025-10-25 10:39:22 +0700

    npm run build

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,30 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      external: [],
+      output: {
+        manualChunks: {
+          vendor: ["react", "react-dom"],
+          quill: ["react-quill"],
+          router: ["react-router-dom"],
+          charts: ["recharts"],
+          utils: ["axios", "html2pdf.js"],
+        },
+      },
+    },
+    commonjsOptions: {
+      include: [/node_modules/],
+    },
+    target: "esnext",
+  },
   server: {
     proxy: {
-      '/api': {
-        target: 'http://localhost:3000', // <-- PORT BE = 3000
+      "/api": {
+        target: "http://localhost:3000", // <-- PORT BE = 3000
         changeOrigin: true,
         secure: false,
         // nếu BE KHÔNG có prefix /api thì mở dòng dưới:
@@ -14,4 +32,4 @@ export default defineConfig({
       },
     },
   },
-})
+});
